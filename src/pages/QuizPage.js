1fde281749852f62b7580196e072d23719d94147
Your Name <you@example.com>
2025-08-12 22:48:47 +0700

    makasih rehan lagi anjeng

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -105,7 +105,7 @@ const QuizPage = ({ isMusicPlaying: initialMusicPlaying = true }) => {
     } else {
       return {
         emoji: '🥉',
-        message: '💪 Semangat goblok!',
+        message: '💪 Semangat goblok! rehan ngancurin github',
         color: 'bg-red-100 text-red-800'
       };
     }
